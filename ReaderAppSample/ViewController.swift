27166397729e96bd6062bfc692bdf30d69c5c773
Okawa Aoi <okawa.a@nexceed.co.jp>
2019-06-18 18:15:02 +0900

    Initial Commit

---
+++ ReaderAppSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReaderAppSample
+//
+//  Created by 大川葵 on 2019/06/18.
+//  Copyright © 2019 Aoi Okawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
